6cdb49facd613c93f430779b8840cd8cdc9bd443
alexitmaster88 <alexitmaster88@gmail.com>
2025-06-26 15:51:40 +0500

    Fix Amplify static deployment with output: export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export', // Required for Amplify static hosting
 };
 
 module.exports = nextConfig;
